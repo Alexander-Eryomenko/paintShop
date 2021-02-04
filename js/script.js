e79aa5bd6218db875e3de3545e05cacc17871670
Alexander-Eryomenko <sanyochek1991@gmail.com>
2021-02-04 14:13:57 +0200

    second section-slider

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.popular__slider').slick({
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../img/popular-slider/circle-left.svg" alt="arrowleft"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../img/popular-slider/circle-right.svg" alt="arrowright"></button>',
+    dots: true,
+    slidesToShow: 3,
+    speed: 500,
+  });
+});
